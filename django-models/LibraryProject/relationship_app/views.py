2bde2a00514259a39cf17d82a780263b650ac3c2
ELOSTORA <mohamedramysocial@gmail.com>
2025-11-05 17:43:17 +0200

    task 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,19 +1,23 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from .models import Book, Library
 
-# =========================
-# Function-Based View (FBV)
-# =========================
+# Function-Based View
 def list_books(request):
-    books = Book.objects.all()  # استعلام كل الكتب
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-
-# =========================
-# Class-Based View (CBV)
-# =========================
-class LibraryDetailView(DetailView):
-    model = Library
+# Class-Based View: ListView of books in a specific library
+class LibraryBooksView(ListView):
+    model = Book
     template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        library = get_object_or_404(Library, pk=self.kwargs['pk'])
+        return Book.objects.filter(library=library)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['library'] = get_object_or_404(Library, pk=self.kwargs['pk'])
+        return context
