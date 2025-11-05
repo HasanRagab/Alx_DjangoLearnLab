8213ad455cbe6d1629d25235561d670d3546519e
ELOSTORA <mohamedramysocial@gmail.com>
2025-11-05 16:57:05 +0200

    Fixed librarian query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,44 +1,22 @@
-import os
 import django
+import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    books = Book.objects.filter(author=author)
-    for book in books:
-        print(book.title)
+author_name = "John Doe"
+books_by_author = Book.objects.filter(author__name=author_name)
+print("Books by", author_name, ":", [book.title for book in books_by_author])
 
 # List all books in a library
-def list_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    for book in library.books.all():
-        print(book.title)
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+print("Books in", library_name, ":", [book.title for book in books_in_library])
 
-# Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    print(library.librarian.name)
-
-
-if __name__ == "__main__":
-    # Optional test data (to check queries)
-    author = Author.objects.create(name="George Orwell")
-    book1 = Book.objects.create(title="1984", author=author)
-    book2 = Book.objects.create(title="Animal Farm", author=author)
-    library = Library.objects.create(name="Central Library")
-    library.books.add(book1, book2)
-    librarian = Librarian.objects.create(name="Ahmad", library=library)
-
-    print("Books by author:")
-    get_books_by_author("George Orwell")
-
-    print("\nBooks in library:")
-    list_books_in_library("Central Library")
-
-    print("\nLibrarian for library:")
-    get_librarian_for_library("Central Library")
+# ✅ Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)
+print("Librarian for", library_name, ":", librarian.name)
