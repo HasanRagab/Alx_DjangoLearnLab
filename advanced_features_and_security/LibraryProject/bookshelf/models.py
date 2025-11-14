f98865a2e81902f6349b830b8023e64ac1623194
ELOSTORA <mohamedramysocial@gmail.com>
2025-11-14 19:15:34 +0200

    calss not found

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.apps import apps
+from django.conf import settings
+
+# Reference the actual custom user model so static tools can resolve it:
+CustomUser = apps.get_model(settings.AUTH_USER_MODEL)
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
