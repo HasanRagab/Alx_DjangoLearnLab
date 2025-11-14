caa1b4a53b5dabd6fe49dc076ccdefd95c51b926
ELOSTORA <mohamedramysocial@gmail.com>
2025-11-14 20:25:33 +0200

    fix1

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+# VIEW — Only users with can_view can see books
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+# CREATE — Only users with can_create
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    return render(request, 'bookshelf/create_book.html')
+
+# EDIT — Only users with can_edit
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
+
+# DELETE — Only users with can_delete
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'bookshelf/delete_book.html', {'book': book})
